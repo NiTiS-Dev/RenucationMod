6bbbca388a4979687d7241d1c61d3cea03601c7f
NickName73 <egorooz73@gmail.com>
2022-06-02 16:45:38 +0300

    Pull files

---
+++ Renucation.cs
@@ -0,0 +1,13 @@
+using Terraria.ModLoader;
+
+namespace Renucation
+{
+	public class Renucation : Mod
+	{
+		public const string AssetPath = $"{nameof(Renucation)}/Assets/";
+		public override void Unload()
+		{
+
+		}
+	}
+}
