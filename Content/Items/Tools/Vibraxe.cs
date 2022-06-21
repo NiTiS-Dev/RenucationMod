f09fbf25b722478f8796336ea77f3b3713d01000
NickName73 <egorooz73@gmail.com>
2022-06-21 23:19:01 +0300

    Added glow mask

--- Content/Items/Tools/Vibraxe.cs
+++ Content/Items/Tools/Vibraxe.cs
@@ -1,6 +1,8 @@
 ﻿// The NiTiS-Dev licenses this file to you under the MIT license.
 
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.ObjectModel;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -28,6 +30,26 @@ public class Vibraxe : ModItem
 		Item.autoReuse = true;
 
 		Item.pick = 195;
+	}
+	public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
+	{
+		Texture2D texture = Mod.Assets.Request<Texture2D>("Content/Items/Tools/Vibraxe_Glow").Value;
+		spriteBatch.Draw
+		(
+			texture,
+			new Vector2
+			(
+				Item.position.X - Main.screenPosition.X + Item.width * 0.5f,
+				Item.position.Y - Main.screenPosition.Y + Item.height - texture.Height * 0.5f + 2f
+			),
+			new Rectangle(0, 0, texture.Width, texture.Height),
+			Color.White,
+			rotation,
+			texture.Size() * 0.5f,
+			scale,
+			SpriteEffects.None,
+			0f
+		);
 	}
 	public override void MeleeEffects(Player player, Rectangle hitbox)
 	{
