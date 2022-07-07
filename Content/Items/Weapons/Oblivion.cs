8550b70796b24db2ef3ac668e5d996a3e30b7ff1
NickName73 <egorooz73@gmail.com>
2022-07-08 00:38:13 +0300

    ф

---
+++ Content/Items/Weapons/Oblivion.cs
@@ -0,0 +1,13 @@
+﻿// The NiTiS-Dev licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renucation.Content.Items.Weapons;
+public class Oblivion : ModItem
+{
+
+}
